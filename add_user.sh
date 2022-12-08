336461e0d8076af95f08213806541000c639d754
francois <francois.bourgeois@tuta.io>
2022-12-08 09:53:45 +0100

    Script complet

--- add_user.sh
+++ add_user.sh
@@ -2,24 +2,27 @@
 echo "Ajout d'un nouvel utilisateur"
 echo "ATTENTION les caractères spéciaux peuvent causer des problèmes !!!"
 read -p "Nom d'utilisateur : " username
-read -sp "Clé publique : " ssh_key
+read -p "Nom du groupe : " groupname
+read -p "Mot de passe : " password
+read -p "Clé publique : " ssh_key
 echo ''
-
-echo "Création de l'utilisateur"
-sudo useradd $username --create-home -G sftp_users 
-echo "Ajout de francois au groupe créé pour l'accès aux documents"
-sudo usermod -G $username francois
-echo "On change le nivea d'accès au dossier de l'utilisateur créé"
+# Création de l'utilisateur
+sudo useradd $username --create-home
+sudo groupadd $groupname
+sudo usermod -g $groupname $username 
+sudo usermod -aG sftp_users $username
+# On change le mot de passe de l'utilisateur
+echo -e '$password\n$password' | sudo passwd $username
+# Ajout de francois au groupe créé pour l'accès aux documents
+sudo usermod -aG $groupname francois
+# On change le niveau d'accès au dossier de l'utilisateur créé
 sudo chmod 770 /home/$username
-echo "Ajout du dossier pour les clés ssh"
-cd /home/$username/
-mkdir .ssh
-sudo chgrp $username .ssh
-sudo chown $username .ssh
-cd ./.ssh/
-echo $ssh_key > authorized_keys
-sudo chgrp $username authorized_keys
-sudo chown $username authorized_keys
-sudo chmod 600 authorized_keys
-cd ..
-sudo chmod 700 .ssh
+# Ajout du dossier pour les clés ssh
+mkdir /home/$username/.ssh
+sudo chgrp $groupname /home/$username/.ssh
+sudo chown $username /home/$username/.ssh
+echo $ssh_key > /home/$username/.ssh/authorized_keys
+sudo chgrp $groupname /home/$username/.ssh/authorized_keys
+sudo chown $username /home/$username/.ssh/authorized_keys
+sudo chmod 600 /home/$username/.ssh/authorized_keys
+sudo chmod 700 /home/$username/.ssh
