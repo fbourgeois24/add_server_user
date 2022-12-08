6378df542feca66399b6ed538e7ffaae5ad0859d
francois <francois.bourgeois@tuta.io>
2022-12-08 10:42:10 +0100

    Ajout suppression groupe initial et suppression sudo

--- add_user.sh
+++ add_user.sh
@@ -9,22 +9,23 @@ read -p "Mot de passe : " password
 read -p "Clé publique : " ssh_key
 echo ''
 # Création de l'utilisateur
-sudo useradd $username --create-home
-sudo groupadd $groupname
-sudo usermod -g $groupname $username 
-sudo usermod -aG sftp_users $username
+useradd $username --create-home
+groupadd $groupname
+usermod -g $groupname $username
+groupdel $username
+usermod -aG sftp_users $username
 # On change le mot de passe de l'utilisateur
-echo -e '$password\n$password' | sudo passwd $username
+echo -e '$password\n$password' | passwd $username
 # Ajout de francois au groupe créé pour l'accès aux documents
-sudo usermod -aG $groupname francois
+usermod -aG $groupname francois
 # On change le niveau d'accès au dossier de l'utilisateur créé
-sudo chmod 770 /home/$username
+chmod 770 /home/$username
 # Ajout du dossier pour les clés ssh
 mkdir /home/$username/.ssh
-sudo chgrp $groupname /home/$username/.ssh
-sudo chown $username /home/$username/.ssh
+chgrp $groupname /home/$username/.ssh
+chown $username /home/$username/.ssh
 echo $ssh_key > /home/$username/.ssh/authorized_keys
-sudo chgrp $groupname /home/$username/.ssh/authorized_keys
-sudo chown $username /home/$username/.ssh/authorized_keys
-sudo chmod 600 /home/$username/.ssh/authorized_keys
-sudo chmod 700 /home/$username/.ssh
+chgrp $groupname /home/$username/.ssh/authorized_keys
+chown $username /home/$username/.ssh/authorized_keys
+chmod 600 /home/$username/.ssh/authorized_keys
+chmod 700 /home/$username/.ssh
